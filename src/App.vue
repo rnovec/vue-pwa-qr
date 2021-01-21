3f50fa341a960b705fc862eaba6390fb4d2fea2d
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-01-20 20:58:46 -0600

    QR

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,96 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+  <v-app id="inspire">
+    <v-system-bar app>
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app clipped-right flat height="72">
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="156">
+        <v-text-field
+          dense
+          flat
+          hide-details
+          append-icon="mdi-magnify"
+          rounded
+          solo-inverted
+        ></v-text-field>
+      </v-responsive>
     </v-app-bar>
 
+    <v-navigation-drawer v-model="drawer" app width="300">
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        color="grey lighten-3"
+        mini-variant
+      >
+        <v-avatar
+          class="d-block text-center mx-auto mt-4"
+          color="grey darken-1"
+          size="36"
+        ></v-avatar>
+
+        <v-divider class="mx-3 my-5"></v-divider>
+
+        <v-avatar
+          v-for="n in 6"
+          :key="n"
+          class="d-block text-center mx-auto mb-9"
+          color="grey lighten-1"
+          size="28"
+        ></v-avatar>
+      </v-navigation-drawer>
+
+      <v-sheet color="grey lighten-5" height="128" width="100%"></v-sheet>
+
+      <v-list class="pl-14" shaped>
+        <v-list-item v-for="n in 10" :key="n" link>
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer app clipped right>
+      <v-list>
+        <v-list-item v-for="n in 5" :key="n" link>
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
+      {{ result }}
+      <vue-qrcode value="https://www.1stg.me" />
+
       <v-dialog v-model="dialog" width="500">
-        <template v-slot:activator="{ on, attrs }">
+        <!-- <template v-slot:activator="{ on, attrs }">
           <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
             Click Me
           </v-btn>
-        </template>
+        </template> -->
 
         <v-card>
           <v-card-title class="headline grey lighten-2">
             Privacy Policy
           </v-card-title>
 
-          <v-card-text>
-            <StreamBarcodeReader
-              v-if="dialog"
-              @decode="onDecode"
-              @loaded="onLoaded"
-            ></StreamBarcodeReader>
-          </v-card-text>
-
-          <v-divider></v-divider>
+          <StreamBarcodeReader
+            v-if="dialog"
+            @decode="getResult"
+            @loaded="onLoaded"
+          ></StreamBarcodeReader>
 
           <v-card-actions>
             <v-spacer></v-spacer>
@@ -65,38 +101,47 @@
         </v-card>
       </v-dialog>
     </v-main>
+
+    <v-footer app color="transparent" height="72" inset>
+      <v-text-field
+        background-color="grey lighten-1"
+        dense
+        flat
+        hide-details
+        append-icon="mdi-qrcode"
+        @click:append="dialog = true"
+        rounded
+        solo
+      ></v-text-field>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 import { StreamBarcodeReader } from 'vue-barcode-reader'
+import VueQrcode from 'vue-qrcode'
 
 export default {
-  name: 'App',
-
+  data: () => ({ drawer: null, dialog: false, mediaStream: null, result: '' }),
   components: {
-    StreamBarcodeReader
+    StreamBarcodeReader,
+    VueQrcode
   },
-
-  data: () => ({
-    dialog: false,
-    text: ''
-  }),
-
   methods: {
-    onDecode (val) {
+    getResult (val) {
       if (val) {
-        this.text = val
+        this.result = val
         this.dialog = false
-        // llamar API
       }
     },
     onLoaded () {
-      console.log('Loaded!')
-    },
-    onError(error) {
-      console.log(error)
+      console.log('loaded')
     }
+  },
+  mounted () {
+    // navigator.mediaDevices.getUserMedia({ video: true }).then(mediaStream => {
+    //   this.mediaStream = mediaStream
+    // })
   }
 }
 </script>
