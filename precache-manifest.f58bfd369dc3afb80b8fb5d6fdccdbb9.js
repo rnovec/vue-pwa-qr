925164cedbdd20152a37134e7ebb4a03bbff0138
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-01-20 21:01:59 -0600

    Updates

---
+++ precache-manifest.f58bfd369dc3afb80b8fb5d6fdccdbb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7890dd84eb98f0cd44",
+    "url": "/css/chunk-vendors.afe01af6.css"
+  },
+  {
+    "revision": "fd80613c7e73e82641642a068ef4db7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90ea1258fccbb51839f3",
+    "url": "/js/app.d4ecba38.js"
+  },
+  {
+    "revision": "5e7890dd84eb98f0cd44",
+    "url": "/js/chunk-vendors.e5dc69d7.js"
+  },
+  {
+    "revision": "a76feb07b34530f498b824651e234c7e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "65dc94066f60f9c6233a1522e553b772",
+    "url": "/service-worker.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,40 @@
+importScripts("/precache-manifest.f58bfd369dc3afb80b8fb5d6fdccdbb9.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.setConfig({
+  debug: false
+})
+
+workbox.precaching.precacheAndRoute([])
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
+  })
+)
+
+// workbox.routing.registerRoute(
+//   new RegExp('https://digital-window.herokuapp.com'),
+//   workbox.strategies.networkFirst({
+//     cacheName: 'api'
+//   })
+// )
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ]
+  })
+)
+
