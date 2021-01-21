175a5ffb8fa4f20aa20cc9d257f44c037ab5bc88
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-01-20 20:13:18 -0600

    Leer QR, Barcode

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -38,23 +34,69 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
+            Click Me
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="headline grey lighten-2">
+            Privacy Policy
+          </v-card-title>
+
+          <v-card-text>
+            <StreamBarcodeReader
+              v-if="dialog"
+              @decode="onDecode"
+              @loaded="onLoaded"
+            ></StreamBarcodeReader>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false">
+              I accept
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import { StreamBarcodeReader } from 'vue-barcode-reader'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    StreamBarcodeReader
   },
 
   data: () => ({
-    //
+    dialog: false,
+    text: ''
   }),
-};
+
+  methods: {
+    onDecode (val) {
+      if (val) {
+        this.text = val
+        this.dialog = false
+        // llamar API
+      }
+    },
+    onLoaded () {
+      console.log('Loaded!')
+    },
+    onError(error) {
+      console.log(error)
+    }
+  }
+}
 </script>
